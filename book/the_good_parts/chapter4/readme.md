bb123c8c9ed83fe0b1d3d18cb1be74e6015e2cc6
JYL827 <1345044954@qq.com>
2020-07-08 21:46:33 +0800

    补充注释

---
+++ book/the_good_parts/chapter4/readme.md
@@ -0,0 +1,7 @@
+## vscode 开发环境
+
+1.网页 web server 浏览 https://
+ live-server 可以立刻自动提供web server
+ npm i -g live-server
+ npm node的包管理器； -g global
+ 
--- html/video/index.js
+++ html/video/index.js
@@ -9,12 +9,14 @@ function handlemove(e) {
     var min = 0.4
     var max = 4
     var height = Math.round(percent * 100) + '%'
-    // 调用四舍五入函数
+    // 调用四舍五入函数并化为百分数，与speed-bar的height设定高度单位相同
     var playbackrate = percent * (max - min) + min
     bar.style.height = height
     bar.textContent = playbackrate.toFixed(2) + 'x'
+    // 把playbackrate的值赋给bar的文本信息
+    // toFixed(2) 保留两位小数
     video.playbackRate = playbackrate
 }
 
 speed.addEventListener('mousemove', handlemove)
-// 用监听器监听鼠标移动事件并触发方法
+// 用监听器监听鼠标移动事件并触发函数handlemove
