b970be2e0b411703cc88140adcf92978cc20a3cf
JYL827 <1345044954@qq.com>
2020-07-30 14:52:00 +0800

    小程序请求数据

--- wxapp/doubanmovie/pages/home/home.js
+++ wxapp/doubanmovie/pages/home/home.js
@@ -1,66 +1,127 @@
 // pages/home/home.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    allMovies: [
+      {
+        title: "院线热映",
+        url: "/v2/movie/in_theaters",
+        movies: []
+      },
+      {
+        title: "新片榜",
+        url: "/v2/movie/new_movies",
+        movies: []
+      },
+      {
+        title: "口碑榜",
+        url: "/v2/movie/weekly",
+        movies: []
+      },
+      {
+        title: "北美票房榜",
+        url: "/v2/movie/us_box",
+        movies: []
+      },
+      {
+        title: "Top250",
+        url: "/v2/movie/top250",
+        movies: []
+      }
 
+    ]
   },
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.getCity((city) => {
-      this.loadData(0,{city:city, apikey:'0df993c66c0c636e29ecbb5344252a4a'})
+      this.loadData(0, { city: city, apikey: '0df993c66c0c636e29ecbb5344252a4a' })
     })
+    this.loadData(1, { apikey: '0df993c66c0c636e29ecbb5344252a4a' })
+    this.loadData(2, { apikey: '0df993c66c0c636e29ecbb5344252a4a' })
+    this.loadData(3, { apikey: '0df993c66c0c636e29ecbb5344252a4a' })
+    this.loadData(4, { apikey: '0df993c66c0c636e29ecbb5344252a4a' })
   },
-  loadData(idx, params){
-    let url = wx.db.url('/v2/movie/in_theaters')
-    wx.request({
-      url: url,
-      data: params,
-      header: {'content-type': 'json'},
+
+
+
+  getCity(succeed) {
+    //拿到当前所在的城市名
+    // 先拿到当前所在区域的经纬度(API)
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
       success: (res) => {
         console.log(res)
-      }
-    })
-  },
-  getCity(succeed){
-    // 拿到当前所在的城市名称
-    // 先拿到当前所在区域的经纬度(小程序自带API)
-    wx.getLocation({
-      success: function(res){
-        // console.log(res)
-        // 将经纬度转换成具体的名称
+        // 将经纬度转换成具体的城市名
         wx.request({
           url: 'https://api.map.baidu.com/reverse_geocoding/v3',
           data: {
-            ak: 'AgstQBQMuypsnELAm0YdNXfSjdHC9Xld',
+            ak: 'uXDG6lrpKFg36aUkGnzzcrizuPfTdcVH',
             output: 'json',
             coordtype: 'wgs84ll',
             location: `${res.latitude},${res.longitude}`
-          }, // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-          // header: {}, // 设置请求的 header
-          success: function(res){
-            // console.log(res)
+          },
+          success: (res) => {
+            console.log(res)
             let city = res.data.result.addressComponent.city
-            succeed && succeed(city)
+            // succeed && succeed(city)
+            if (succeed) {
+              succeed(city)
+            }
           },
-          fail: function() {
-            console.log('获取地理位置失败')
-          },
-          complete: function() {
-            // complete
+          fail: () => {
+            console.log('获取城市失败')
           }
         })
       },
-      fail: function() {
-        // fail
+      fail: () => {
       },
     })
   },
+
+
+
+  loadData(idx, params) {
+    let obj = this.data.allMovies[idx]
+    let url = wx.db.url(obj.url)
+    wx.request({
+      url: url,
+      data: params,
+      header: { 'content-type': 'json' },
+      success: (res) => {
+        console.log(res)
+        let movies = res.data.subjects
+        obj.movies = []
+        for (let index = 0; index < movies.length; index++) {
+          let element = movies[index]
+          let movie = element.subject || element
+          // 格式化星星
+          this.updateMovie(movie)
+          obj.movies.push(movie)
+        }
+        this.setData(this.data)
+      }
+    })
+  },
+
+
+
+  updateMovie(movie) {
+    if (!movie.rating.stars) {
+      return
+    }
+    movie.numberStars = parseInt(movie.rating.stars)
+  },
+
+
+
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
