70421575652bb9f68f802c3495b1179f2cb668a9
JYL827 <1345044954@qq.com>
2020-08-06 22:07:28 +0800

    微信读书

---
+++ wxapp/weChatReading/component/book-recommend/book-recommend.js
@@ -0,0 +1,26 @@
+// component/book-recommend/book-recommend.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    book: {
+      type: Object,
+      value: {}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- wxapp/weChatReading/component/searchBar/searchbar.js
+++ wxapp/weChatReading/component/searchBar/searchbar.js
@@ -4,14 +4,39 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    title: {
+      type: Array,
+      value: []
+    },
+    anotherPage: {
+      type: String,
+      value: '书城'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    showTitle: ''
+  },
+  lifetimes: {
+    attached () {
+      let i = 1;
+      this.setData({
+        showTitle: this.data.title[0].name
+      })
+      // 设置每3s更新一次搜索框内容
+      setInterval(() => {
+        this.setData({
+          showTitle: this.data.title[i].name
+        })
+        i++
+        if (i >= this.data.title.length) {
+          i = 0
+        }
+      }, 3000)
+    }
   },
 
   /**
