592bb66de6bec8f06a84485fb65538b7b1e83850
JYL827 <1345044954@qq.com>
2020-08-20 12:09:56 +0800

    ts初识及基础数据类型

---
+++ TS/hello-ts/src/index.ts
@@ -0,0 +1,31 @@
+// function greeter(person: string) {
+//     return "hello" + person
+// }
+// const user = "wn"
+
+
+
+
+// // 类型
+// let isloading: boolean = false
+
+// let book: string = 'typeScript'
+
+// // 空值
+// function warnUser(): void {
+    
+// }
+// const a: void = undefined
+
+// let aa: any = undefined
+
+// let sym1 = Symbol('key1')
+
+// Bigint
+const max = BigInt(Number.MAX_SAFE_INTEGER)
+
+const max1 = max + 1n
+const max2 = max + 2n
+
+console.log(max1 === max2)
+
