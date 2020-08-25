aa6723d1412290d42f5814278cf408f47b9995aa
JYL827 <1345044954@qq.com>
2020-08-25 12:06:24 +0800

    vue-route

---
+++ vue/vue-router/src/components/Hi.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+      <h1>{{msg}}</h1>
+      <router-link :to="{name: 'hi1', params:{name: '乐怡'}}">hi 一</router-link>
+      <span style="display:inline-block;width:50px"></span>
+      <router-link to="/hi/hi2">hi 二</router-link>
+      <router-view></router-view>      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hi',
+    data () {
+        return {
+            msg: 'Hi, I am 蜗牛'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
