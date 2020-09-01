f6477ab07fe6bb1a13aa470d405b99da74d3756c
JYL827 <1345044954@qq.com>
2020-09-01 12:13:44 +0800

    饿了么商家

---
+++ vue/vue-elm/src/components/v-header/v-header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img
+          width="64"
+          height="64"
+          src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1194131577,2954769920&fm=26&gp=0.jpg"
+          alt
+        />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">粥品香坊</span>
+        </div>
+        <div class="description">蜂鸟专送/38分钟送达</div>
+        <div class="support">
+          <img src="./brand@2x.png" alt />
+          <span class="text">在线支付满25减4</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
