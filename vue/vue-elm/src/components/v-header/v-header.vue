42162a4e1a817481426d8461913ceb98e5cef24d
JYL827 <1345044954@qq.com>
2020-09-02 12:21:22 +0800

    饿了么商家

--- vue/vue-elm/src/components/v-header/v-header.vue
+++ vue/vue-elm/src/components/v-header/v-header.vue
@@ -5,19 +5,18 @@
         <img
           width="64"
           height="64"
-          src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1194131577,2954769920&fm=26&gp=0.jpg"
-          alt
+          :src="seller.avatar"
         />
       </div>
       <div class="content">
         <div class="title">
           <span class="brand"></span>
-          <span class="name">粥品香坊</span>
+          <span class="name">{{seller.name}}</span>
         </div>
-        <div class="description">蜂鸟专送/38分钟送达</div>
-        <div class="support">
-          <img class="support-ico" src="./brand@2x.png" alt />
-          <span class="text">在线支付满25减4</span>
+        <div class="description">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
+        <div class="support" v-if="seller.supports">
+          <support-ico :size=1 :type="seller.supports[0].type"></support-ico>
+          <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
     </div>
@@ -25,54 +24,91 @@
 </template>
 
 <script>
-export default {};
+import SupportIco from '@/components/support-ico/support-ico'
+export default {
+  props: {
+    seller: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  components: {
+    SupportIco
+  }
+};
 </script>
 
 <style lang="stylus">
-@import "../../common/stylus/variable.styl";
-@import "../../common/stylus/mixin.styl";
-  .header
-    position relative
-    overflow hidden
-    color $color-white
-    background $color-background-ss
-  .content-wrapper
-    position relative
-    display flex
-    align-items center
-    padding 24px 12px 18px 24px
-    .avatar
-      flex 0 0 64px
-      width 64px
-      margin-right 16px
-      img
-        border-radius 2px
-    .content
-      flex 1
-      .title
-        display flex
-        align-items center
-        margin-bottom 8px
-        .brand
-          width 30px
-          height 18px
-          bg-image('brand')
-          background-size 30px 18px
-          background-repeat no-repeat
-        .name
-          margin-left 5px
-          font-size $fontsize-large
-          font-weight bold
-      .description
-        margin-bottom 8px
-        line-height 12px
-        font-size $fontsize-small = 12px
-      .support
-        display flex
-        align-items center
-        &-ico
-          margin-right 4px
-        .text
-          line-height 12px
-          font-size $fontsize-small-s = 10px
+@import '../../common/stylus/variable.styl';
+@import '../../common/stylus/mixin.styl';
+
+.header {
+  position: relative;
+  overflow: hidden;
+  color: $color-white;
+  background: $color-background-ss;
+}
+
+.content-wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 24px 12px 18px 24px;
+
+  .avatar {
+    flex: 0 0 64px;
+    width: 64px;
+    margin-right: 16px;
+
+    img {
+      border-radius: 2px;
+    }
+  }
+
+  .content {
+    flex: 1;
+
+    .title {
+      display: flex;
+      align-items: center;
+      margin-bottom: 8px;
+
+      .brand {
+        width: 30px;
+        height: 18px;
+        bg-image('brand');
+        background-size: 30px 18px;
+        background-repeat: no-repeat;
+      }
+
+      .name {
+        margin-left: 5px;
+        font-size: $fontsize-large;
+        font-weight: bold;
+      }
+    }
+
+    .description {
+      margin-bottom: 8px;
+      line-height: 12px;
+      font-size: $fontsize-small = 12px;
+    }
+
+    .support {
+      display: flex;
+      align-items: center;
+
+      &-ico {
+        margin-right: 4px;
+      }
+
+      .text {
+        line-height: 12px;
+        font-size: $fontsize-small-s = 10px;
+      }
+    }
+  }
+}
 </style>
